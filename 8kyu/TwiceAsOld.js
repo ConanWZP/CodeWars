fd44f27bc792f660b000ff8eec9c8ca07257df19
ConanWZP <yaroslav2281337@mail.ru>
2023-05-04 13:45:09 +0300

    twiceAsOld

---
+++ 8kyu/TwiceAsOld.js
@@ -0,0 +1,19 @@
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+
+    if (sonYearsOld !== 0) {
+        let simpleDivide = dadYearsOld / sonYearsOld
+        if (simpleDivide > 2) {
+            return dadYearsOld - 2 * sonYearsOld
+        } else if (simpleDivide < 2) {
+            return 2 * sonYearsOld - dadYearsOld
+        } else if (simpleDivide === 2) {
+            return 0
+        }
+    } else {
+        return dadYearsOld
+    }
+}
+
+twiceAsOld(29, 0)
+
+console.log(twiceAsOld(29, 0))
