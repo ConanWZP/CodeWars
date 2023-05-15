3ca7a0172f2c22f68cdc887f1c25690ded5019ce
ConanWZP <yaroslav2281337@mail.ru>
2023-05-15 20:58:18 +0300

    Length and two values

---
+++ 7kyu/Length and two values.js
@@ -0,0 +1,17 @@
+function alternate(n, firstValue, secondValue){
+    let array = []
+
+    for (let i = 0; i < n; i++) {
+        if (i % 2 === 0) {
+            array.push(firstValue)
+        } else {
+            array.push(secondValue)
+        }
+
+    }
+
+    return array
+
+}
+
+console.log( alternate(0, "lemons", "apples")  )
