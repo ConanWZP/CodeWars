4338b4592d8110617ed1df9925af8fb58a373a04
ConanWZP <yaroslav2281337@mail.ru>
2023-05-15 21:45:20 +0300

    Did she say hallo

--- 8kyu/Did she say hallo.js
+++ 8kyu/Did she say hallo.js
@@ -1,16 +1,16 @@
 function validateHello(greetings) {
 
     let array = ['hello', 'ciao', 'salut', 'hallo', 'hola', 'ahoj', 'czesc']
-    let count = 0
+
 
     for (let i = 0; i < array.length; i++) {
         let str = greetings.toLowerCase()
         if (str.includes(array[i])) {
-            count++
+            return true
         }
     }
+    return false
 
-    return count > 0
 }
 
-console.log(validateHello('heLsLO: wie; lA: HOMBRE: ArE? ViSTA; viSTa, yOu, haSTA, hOw? TScHuSS HEldLO, lA TscHUSs; Are: HOmBrE BIeN. HoW'))
+console.log(validateHello('heLLO: wie; lA: HOMBRE: ArE? ViSTA; viSTa, yOu, haSTA, hOw? TScHuSS HEldLO, lA TscHUSs; Are: HOmBrE BIeN. HoW'))
