9e412c19f0a06bceb4a911f6b99c18965f488123
ConanWZP <yaroslav2281337@mail.ru>
2023-05-19 12:18:10 +0300

    Calculate BMI

---
+++ 8kyu/Calculate BMI.js
@@ -0,0 +1,15 @@
+function bmi(weight, height) {
+
+    let b = weight/(height**2)
+
+    if (b <= 18.5) {
+        return 'Underweight'
+    } else if (b <= 25) {
+        return 'Normal'
+    } else if (b <=30) {
+        return 'Overweight'
+    } else {
+        return 'Obese'
+    }
+
+}
