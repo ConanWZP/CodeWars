12d1bbe0da786a89a2e5f45b86d30fa4cf7d9f98
ConanWZP <yaroslav2281337@mail.ru>
2023-06-01 12:36:20 +0300

    Triple Trouble

---
+++ 8kyu/Triple Trouble.js
@@ -0,0 +1,13 @@
+function tripleTrouble(one, two, three){
+    let str = '';
+    for (let i = 0; i < one.length; i++) {
+        str+= one[i]
+        str+= two[i]
+        str+= three[i]
+    }
+    return str
+
+}
+
+
+console.log(      tripleTrouble("LLh","euo","xtr")      )
