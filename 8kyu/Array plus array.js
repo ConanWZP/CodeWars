8a63f304ff6bc9cad08a093c693c4ce44e02994e
ConanWZP <yaroslav2281337@mail.ru>
2023-06-02 12:41:51 +0300

    Array plus array

---
+++ 8kyu/Array plus array.js
@@ -0,0 +1,12 @@
+function arrayPlusArray(arr1, arr2) {
+    let sum = 0;
+    for (let i = 0; i < arr1.length; i++) {
+        sum += arr1[i]
+    }
+    for (let j = 0; j < arr2.length; j++) {
+        sum += arr2[j]
+    }
+    return sum; //something went wrong
+}
+
+console.log(   arrayPlusArray([1, 2, 3], [4, 5, 6])      )
