731fa86f76c09c8b756c80c74148037dce16c3d0
ConanWZP <yaroslav2281337@mail.ru>
2023-06-02 12:44:06 +0300

    Shifty Closures

---
+++ 8kyu/Shifty Closures.js
@@ -0,0 +1,11 @@
+var name1 = 'Abe';
+var greet_abe = function() {
+    return "Hello, " + name1 + '!';
+};
+
+name2 = 'Ben';
+var greet_ben = function() {
+    return "Hello, " + name2 + '!';
+};
+
+console.log(   greet_abe()   )
