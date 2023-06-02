addd4e6fcbbf634b3597d8ec13bc182282a65e3e
ConanWZP <yaroslav2281337@mail.ru>
2023-06-02 13:00:31 +0300

    Drink about

---
+++ 8kyu/Counting sheep....js
@@ -0,0 +1,11 @@
+function countSheeps(arrayOfSheep) {
+
+    let sum = 0
+    for (let i = 0; i < arrayOfSheep.length; i++) {
+        if (arrayOfSheep === true) {
+            sum += 1
+        }
+    }
+    return sum
+
+}
