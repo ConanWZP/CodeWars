134bdf149cfe3d9047058e22450079066b7ccbf3
ConanWZP <yaroslav2281337@mail.ru>
2023-06-06 13:22:32 +0300

    No Loops 2 - You only need one

---
+++ 8kyu/No Loops 2 - You only need one.js
@@ -0,0 +1,8 @@
+function check(a,x){
+    /*let b = a.filter((e) => e === x)
+    console.log(b)*/
+    return a.includes((e) => e === a)
+   // return b.length === 0 ? false : true
+};
+
+console.log(   check([66, 101], 66)    )
