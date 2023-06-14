6e02f6c5fd193409b0728ab229b99d5e8f092959
ConanWZP <yaroslav2281337@mail.ru>
2023-06-14 14:59:03 +0300

    Switch/Case - Bug Fixing #6

---
+++ 8kyu/Switch Case - Bug Fixing #6.js
@@ -0,0 +1,15 @@
+function evalObject(value){
+    var result = 0;
+    switch(value.operation){
+        case'+': return result = value.a + value.b;
+        case'-': return result = value.a - value.b;
+        case'/': return result = value.a / value.b;
+        case'*': return result = value.a * value.b;
+        case'%': return result = value.a % value.b;
+        case'^': return result = Math.pow(value.a, value.b);
+    }
+
+}
+
+
+console.log(    evalObject(   {a:1,b:1,operation:'+'}     )        )
