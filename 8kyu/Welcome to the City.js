344e1c4d0c1ca0de25cb4c074fd6b5c2703c61ad
ConanWZP <yaroslav2281337@mail.ru>
2023-08-16 15:39:41 +0300

    Welcome to the City

---
+++ 8kyu/Welcome to the City.js
@@ -0,0 +1,8 @@
+function sayHello( name, city, state ) {
+
+    return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+
+}
+
+
+console.log(      sayHello(['John', 'Smith'], 'Phoenix', 'Arizona')       )
