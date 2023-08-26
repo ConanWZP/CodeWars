79c11778bf73aafe5d88793e34a3a0a3c71d25b1
ConanWZP <yaroslav2281337@mail.ru>
2023-08-26 12:52:05 +0300

    Stringy Strings

---
+++ 8kyu/Stringy Strings.js
@@ -0,0 +1,13 @@
+function stringy(size) {
+    let str = ''
+    for (let i=1; i <= size; i++) {
+        if (i % 2 === 0) {
+            str += '0'
+        } else {
+            str += '1'
+        }
+    }
+    return str
+}
+
+console.log(  stringy(4)  )
