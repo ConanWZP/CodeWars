5bb98cdc2dd93aeffc1cf2ccc3e93f0624cde4ff
ConanWZP <yaroslav2281337@mail.ru>
2024-01-01 13:11:25 +0300

    Gradually Adding Parameters

---
+++ 7kyu/Gradually Adding Parameters.js
@@ -0,0 +1,12 @@
+function add() {
+
+    return [...arguments].reduce((acc, item, currentIndex) => {
+        acc += item * (currentIndex + 1)
+        return acc
+    }, 0)
+
+
+}
+
+
+console.log(add(100, 200, 300))
